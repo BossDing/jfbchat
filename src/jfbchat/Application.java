7bc3d09482607fdc5d47649d413fa4d6282103d4
digitex <digitex@63ac1e3e-94f0-4373-9922-ba441b25de4c>
2011-02-12 17:35:39 +0000

    fixed some bugs

--- src/jfbchat/Application.java
+++ src/jfbchat/Application.java
@@ -28,7 +28,7 @@ import javax.swing.*;
 
 
 public class Application {
-    public final String VERSION = "0.01";
+    public final static String VERSION = "0.01";
     private MainFrame mainFrame;
 
     public Application(){
