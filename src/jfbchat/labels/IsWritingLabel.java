073cbfa4e73c1f34e7d2624c74587778315a97e8
digitex <digitex@63ac1e3e-94f0-4373-9922-ba441b25de4c>
2011-05-28 09:33:39 +0000

    Changed the icon image path with a constant in the Img class

--- src/jfbchat/labels/IsWritingLabel.java
+++ src/jfbchat/labels/IsWritingLabel.java
@@ -1,9 +1,9 @@
-/*###################################################
-
+/*#############################################################################
 
 JFBChat it's a simple software written in Java that let you conncted with 
 yours Facebook friends without your browser.
-Copyright (C) 2011  Digitex (Giuseppe Federico)This program is free software: you can redistribute it and/or modify
+Copyright (C) 2011  Digitex (Giuseppe Federico)This program is free software:
+ * you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
@@ -14,27 +14,13 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>. To change this template, choose Tools | Templates
+along with this program.  If not, see <http://www.gnu.org/licenses/>. 
 
-
-JFBChat it's a simple software written in Java that let you conncted with 
-yours Facebook friends without your browser.
-Copyright (C) 2011  Digitex (Giuseppe Federico)This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>. and open the template in the editor.
-###################################################*/
+##############################################################################*/
 package jfbchat.labels;
 
 import javax.swing.JLabel;
+import jfbchat.resources.Imgs;
 
 /**
  *
@@ -46,7 +32,7 @@ public class IsWritingLabel extends JLabel{
     public IsWritingLabel(){
         super("");
         this.isWriting = false;        
-        this.setIcon(new javax.swing.ImageIcon(getClass().getResource("/jfbchat/imgs/Conversation/iswriting.gif")));
+        this.setIcon(new javax.swing.ImageIcon(getClass().getResource(Imgs.IS_WRITING_ICON)));
         
     }
 
